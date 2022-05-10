8f94f1ed1770c8aa691251f9f72d06f64203c3b8
Michael Cullan <mjcullan@gmail.com>
2022-05-10 15:19:57 -0400

    fix gh-pages.js

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ publish(
  'build', // path to public directory
  {
   branch: 'gh-pages',
-  repo: 'https://github.com/mcullan/laion-ai.github.io.git', // Update to point to your repository
+  repo: 'https://github.com/laion-ai/website', // Update to point to your repository
   user: {
    name: 'Michael Cullan', // update to use your name
    email: 'mjcullan@gmail.com' // Update to use your email
